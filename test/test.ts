b6e696da71335287cd894b2864a025397a35370f
geowarin <geowarin@gmail.com>
2017-06-19 14:08:07 +0200

    Simplify

--- test/test.ts
+++ test/test.ts
@@ -51,13 +51,4 @@ describe("Stream operations", () => {
             .toArray()
     ).toEqual([1, 2, 2, 4])
   });
-
-  it('generator', () => {
-
-    function *gen() {
-      yield 1
-    }
-
-    gen().next()
-  });
 });
